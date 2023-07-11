55ae9a4c3d319630fe9cac836fb840dfa5142b0e
akashdasp <akash.dasp@gmail.com>
2023-07-11 15:57:05 +0530

    requirements add

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+with open("README.md","r",encoding="utf-8") as f:
+    long_description = f.read()
+__version__="0.0,0"
+REPO_NAME="Text_summerizer"
+AUTHOR_USER_NAME="akashdasp"
+SRC_REPO="textSummerizer"
+AUTHOR_EMAIL="akashdasp@gmail.com"
