bebb0cf8a6dda92619a90d599d69f1412302c8c8
akashdasp <akash.dasp@gmail.com>
2023-07-20 12:09:40 +0530

    Data_validation_completed

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
+from textSummerizer.pipeline.stage01_data_ingestion import DataIngestionTraningPipelining
+from textSummerizer.pipeline.stage02_data_validation import DataValidationTraningPipelining
 from textSummerizer.logging import logger
-logger.info("Welcome to the Custom Log")
+
+STAGE_NAME= "Data Ingestion Stage"
+try :
+    logger.info(f'{STAGE_NAME} Streaming Data Ingestion')
+    data_ingestion= DataIngestionTraningPipelining()
+    data_ingestion.main()
+    logger.info(f'{STAGE_NAME} Streaming Data Ingestion Completed')
+except Exception as e :
+    # logger.exception(e)
+    # raise e
+    pass
+
+
+
+STAGE_NAME= "Data Validation Stage"
+try :
+    logger.info(f'Streaming {STAGE_NAME} ')
+    data_ingestion= DataIngestionTraningPipelining()
+    data_ingestion.main()
+    logger.info(f'{STAGE_NAME} Completed')
+except Exception as e :
+    logger.exception(e)
+    raise e
--- src/textSummerizer/conifig/configuration.py
+++ src/textSummerizer/conifig/configuration.py
@@ -1,12 +1,13 @@
 import os
 from textSummerizer.constants import *
 from textSummerizer.utils.common import read_yaml,create_directory
-from textSummerizer.entity import DataIngestionConfig
+from textSummerizer.entity import (DataIngestionConfig,
+                                   DataValidationConfig)
 class ConfigurationManager:
     def __init__(self, config_filepath= CONFIG_FILE_PATH, param_filepath= PARAMS_FILE_PATH):
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(param_filepath)
-        create_directory([self.config.artifact_root])
+        create_directory([self.config.artifacts_root])
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config= self.config.data_ingestion
         create_directory([config.root_dir])
@@ -17,3 +18,15 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir
         )
         return data_ingestion_config
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directory([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES,
+        )
+
+        return data_validation_config
